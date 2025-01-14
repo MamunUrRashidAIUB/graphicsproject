3d8d42a0ade10a8643463a0e8c0548697c396907
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2025-01-14 17:58:05 +0600

    points changes

--- Coxbazar/main.cpp
+++ Coxbazar/main.cpp
@@ -2,64 +2,113 @@
 #include <stdio.h>
 #include <math.h>
 
-// Remove unused variables related to the balloon
-float posx = 0.0f;
-float speed = 0.0f;
-
 void boat()
 {
     ///boat1
-    glColor3f(0,0,0);
+    glColor3f(0, 0, 0);
     glBegin(GL_POLYGON);
 
-    glVertex3i(10,550-100,0);
-    glVertex3i(150+50,550-100,0);
-    glVertex3i(120+50,500-100,0);
-    glVertex3i(30,500-100,0);
+    glVertex3i(200,500, 0);
+    glVertex3i(400,500, 0);
+    glVertex3i(350,450, 0);
+    glVertex3i(250,450, 0);
     glEnd();
-  glColor3f(1,0,0);
+
+    glColor3f(1, 0, 0);
     glBegin(GL_POLYGON);
 
-    glVertex3i(50,550-100,0);
-    glVertex3i(150,550-100,0);
-    glVertex3i(150,600-100,0);
-    glVertex3i(50,600-100,0);
+    glVertex3i(240, 500, 0);
+    glVertex3i(360, 500, 0);
+    glVertex3i(360,540, 0);
+    glVertex3i(240,540, 0);
     glEnd();
-    ///boat2
-    glColor3f(0,0,0);
+//boat 2
+    glColor3f(0, 0, 0);
     glBegin(GL_POLYGON);
 
-    glVertex3i(10+200,550-50,0);
-    glVertex3i(150+50+200,550-50,0);
-    glVertex3i(120+50+200,500-50,0);
-    glVertex3i(30+200,500-50,0);
+    glVertex3i(800,500, 0);
+    glVertex3i(1000,500, 0);
+    glVertex3i(950,450, 0);
+    glVertex3i(850,450, 0);
     glEnd();
-  glColor3f(1,0,0);
+
+    glColor3f(1, 0, 0);
     glBegin(GL_POLYGON);
 
-    glVertex3i(50+200,550-50,0);
-    glVertex3i(150+200,550-50,0);
-    glVertex3i(150+200,600-50,0);
-    glVertex3i(50+200,600-50,0);
+    glVertex3i(850, 500, 0);
+    glVertex3i(950, 500, 0);
+    glVertex3i(950,550, 0);
+    glVertex3i(850,550, 0);
     glEnd();
 }
+void hill() {
+    // Hill 1
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(150, 600);  // Left bottom corner of the triangle
+    glVertex2f(300, 700);  // Peak of the hill (higher point)
+    glVertex2f(300, 600); // Right bottom corner of the triangle
+    glEnd();
+
+
+         // Hill 2
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(200, 600);  // Left bottom corner of the triangle
+    glVertex2f(400, 700);  // Peak of the hill (higher point)
+    glVertex2f(500, 600); // Right bottom corner of the triangle
+    glEnd();
+
+       // Hill 3
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(900, 600);  // Left bottom corner of the triangle
+    glVertex2f(1100, 800);  // Peak of the hill (higher point)
+    glVertex2f(1200, 600); // Right bottom corner of the triangle
+    glEnd();
+
+          // Hill 4
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(1000, 600);  // Left bottom corner of the triangle
+    glVertex2f(1200, 700);  // Peak of the hill (higher point)
+    glVertex2f(1300, 600); // Right bottom corner of the triangle
+    glEnd();
+
+             // Hill 5
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(1200, 600);  // Left bottom corner of the triangle
+    glVertex2f(1300, 800);  // Peak of the hill (higher point)
+    glVertex2f(1400, 600); // Right bottom corner of the triangle
+    glEnd();
+               // Hill 6
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(1300, 600);  // Left bottom corner of the triangle
+    glVertex2f(1400, 700);  // Peak of the hill (higher point)
+    glVertex2f(1500, 600); // Right bottom corner of the triangle
+    glEnd();
+
+}
+
 
 void draw_object() {
     // Sky
     glColor3f(0.6196f, 0.9333f, 0.9960f);
     glBegin(GL_QUADS);
     glVertex2f(0, 1200);
-    glVertex2f(1200, 1200);
-    glVertex2f(1200, 600);
-    glVertex2f(0, 600);
+    glVertex2f(1500, 900);
+    glVertex2f(1500, 900);
+    glVertex2f(0, 900);
     glEnd();
 
     // Sea
     glColor3f(0.0f, 0.5f, 0.8f);
     glBegin(GL_QUADS);
     glVertex2f(0, 600);
-    glVertex2f(1200, 600);
-    glVertex2f(1200, 300);
+    glVertex2f(1500, 600);
+    glVertex2f(1500, 300);
     glVertex2f(0, 300);
     glEnd();
 
@@ -67,16 +116,14 @@ void draw_object() {
     glColor3f(0.9f, 0.8f, 0.6f);
     glBegin(GL_QUADS);
     glVertex2f(0, 300);
-    glVertex2f(1200, 300);
-    glVertex2f(1200, 0);
+    glVertex2f(1500, 300);
+    glVertex2f(1500, 0);
     glVertex2f(0, 0);
     glEnd();
 
-    // Draw the boat
-    glPushMatrix();
-    glTranslatef(posx, 0, 0);
+    // Draw the boats
     boat();
-    glPopMatrix();
+    hill();
 }
 
 void display(void) {
@@ -85,30 +132,11 @@ void display(void) {
     glFlush();
 }
 
-void keyboardFunc(unsigned char key, int x, int y) {
-    switch (key) {
-        case 'a': case 'A':
-            speed = 0.0f;
-            break;
-        case 'w': case 'W':
-            speed += 2.0f;
-            break;
-    }
-}
-
-void update(int value) {
-    posx += speed;
-    if (posx >= 1200)
-        posx = 0;
-    glutPostRedisplay();
-    glutTimerFunc(25, update, 0);
-}
-
 void init(void) {
     glClearColor(0.6196f, 0.9333f, 0.9960f, 1.0f);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0, 1200.0, 0.0, 1200.0);
+    gluOrtho2D(0.0, 1500.0, 0.0, 1200.0);
 }
 
 int main(int argc, char** argv) {
@@ -119,10 +147,8 @@ int main(int argc, char** argv) {
     glutCreateWindow("Sea Beach");
 
     glutDisplayFunc(display);
-    glutKeyboardFunc(keyboardFunc);
 
     init();
-    glutTimerFunc(25, update, 0);
     glutMainLoop();
     return 0;
 }
